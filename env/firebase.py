3603cf6e9c2773fbacff04054365541fadef60a1
Febri Arief <febriarief6661@gmail.com>
2023-09-18 04:02:54 +0700

    hide firebase auth

--- env/firebase.py
+++ env/firebase.py
@@ -1,10 +1,10 @@
 firebaseConfig = {
-    "apiKey": "AIzaSyC6B2cvc8MQRYJtNZxmN7-TYXetIopnvGQ",
-    "authDomain": "python-face-recognition-19119.firebaseapp.com",
-    "projectId": "python-face-recognition-19119",
-    "storageBucket": "python-face-recognition-19119.appspot.com",
-    "messagingSenderId": "551277608213",
-    "appId": "1:551277608213:web:1d72b815b18ce45b17fd1c",
+    "apiKey": "YOUR_API_KEY",
+    "authDomain": "YOUR_AUTH_DOMAIN",
+    "projectId": "YOUR_PROJECT_ID",
+    "storageBucket": "YOUR_STORAGE_BUCKET",
+    "messagingSenderId": "YOUR_MESSAGING_SENDER_ID",
+    "appId": "YOUR_APP_ID",
     "databaseURL": "",
-    "serviceAccount": "python-face-recognition-19119-firebase-adminsdk-wzjju-4319dc8db7.json"
+    "serviceAccount": "YOUR_SERVICE_ACCOUNT"
 }
